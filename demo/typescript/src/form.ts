570949a0c39ce7da26062d999656faaa5a19cb27
Dustin Whitney <dustin.whitney@gmail.com>
2018-12-07 19:50:49 -0500

    demo

---
+++ demo/typescript/src/form.ts
@@ -0,0 +1,56 @@
+import { Applicative2 } from "fp-ts/lib/Applicative"
+import { Functor2 } from "fp-ts/lib/Functor"
+import { compose, flip } from "fp-ts/lib/function"
+import { IO } from "fp-ts/lib/IO"
+import { Lens } from "monocle-ts"
+
+declare module "fp-ts/lib/HKT" {
+  interface URI2HKT2<L,A> {
+    Form: Form<L,A>
+  }
+}
+
+export const URI = "Form"
+
+export type URI = typeof URI
+
+export type UpdateEffect<I> = (_: I) => IO<void> ;
+export type UpdateUI<I> = (_: UpdateEffect<I>) => UI;
+export type UI = string
+
+export class Form<I,A>{
+  readonly _tag: "Form" = "Form"
+  readonly _A!: A 
+  readonly _L!: I
+  readonly _U!: UI 
+  readonly _URI!: URI
+
+  constructor(
+    readonly update: (_: I) => { render: UpdateUI<I>, result: A }
+  ){}
+}
+
+const map = <L, A, B>(form: Form<L, A>, f: (a: A) => B): Form<L, B> => 
+  new Form((input) => {
+    const { render, result } = form.update(input);
+    return { render, result: f(result) } 
+  })
+
+const of = <L,A>(result: A): Form<L, A> => 
+  new Form(() => ({ render: (_) => "", result }))
+
+const ap = <L,A,B>(fab: Form<L,(a: A) => B>, fa: Form<L,A>): Form<L,B> =>
+  new Form((input) => {
+    const obj = fab.update(input);
+    const { render, result } = fa.update(input);
+    return { render, result : obj.result(result) };
+  })
+
+export const focus = <I,J,A>(lens: Lens<I,J>) => (form: Form<J,A>): Form<I,A> => 
+ new Form((input: I) => {
+  const data = form.update(lens.get(input))
+  const render = (onChange: UpdateEffect<I>) =>  data.render(compose(onChange, (flip(lens.set)(input))))
+  return ({ render, result: data.result })
+ })
+
+export const form: Functor2<URI> & Applicative2<URI> = { URI, map, ap, of }
---
+++ demo/typescript/src/model.ts
@@ -0,0 +1,50 @@
+import { Lens } from "monocle-ts"
+import { Newtype, iso } from "newtype-ts"
+
+
+//address
+export interface Street extends Newtype<{readonly Street: unique symbol}, string>{}
+export interface City extends Newtype<{readonly Street: unique symbol}, string>{}
+export interface State extends Newtype<{readonly Street: unique symbol}, string>{}
+export interface Zip extends Newtype<{readonly Street: unique symbol}, number>{}
+export type Address = { readonly street : Street, readonly city: City, readonly state: State, zip: Zip }
+
+export const _street      = Lens.fromProp<Address>()("street");
+export const _city        = Lens.fromProp<Address>()("city");
+export const _state       = Lens.fromProp<Address>()("state");
+export const _zip         = Lens.fromProp<Address>()("zip");
+export const _streetIso   = iso<Street>()
+export const _cityIso     = iso<City>()
+export const _stateIso    = iso<State>()
+export const _zipIso      = iso<Zip>()
+
+
+//person
+export interface FirstName extends Newtype<{readonly Street: unique symbol}, string>{}
+export interface LastName extends Newtype<{readonly Street: unique symbol}, string>{}
+export type Person = { readonly firstName: FirstName, readonly lastName: LastName, readonly address: Address }
+
+export const _firstName     = Lens.fromProp<Person>()("firstName");
+export const _lastName      = Lens.fromProp<Person>()("lastName");
+export const _address       = Lens.fromProp<Person>()("address");
+export const _firstNameIso  = iso<FirstName>()
+export const _lastNameIso   = iso<LastName>()
+
+/*
+const address: Address = 
+  { street : _streetIso.wrap("298 Waverly Ave.")
+  , city   : _cityIso.wrap("Brooklyn")
+  , state  : _stateIso.wrap("NY")
+  , zip    : _zipIso.wrap(11205)
+  }
+
+const person: Person = 
+  { firstName : _firstNameIso.wrap("Dustin")
+  , lastName  : _lastNameIso.wrap("Whitney")
+  , address   : address
+  }
+
+const brooklynLens = _address.compose(_city).composeIso(_cityIso)
+
+console.log(brooklynLens.get(person))
+*/
